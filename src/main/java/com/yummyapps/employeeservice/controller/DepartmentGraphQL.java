b271ab41f07d181cc8bcf6e47f954c13e8028d7a
yumycoding <faisalshahzad@yumyapps.com>
2022-06-28 00:50:12 -0700

    Graphql Project Phase two.

--- src/main/java/com/yummyapps/employeeservice/controller/DepartmentGraphQL.java
+++ src/main/java/com/yummyapps/employeeservice/controller/DepartmentGraphQL.java
@@ -6,10 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-<<<<<<< HEAD
+
 import org.springframework.security.access.prepost.PreAuthorize;
-=======
->>>>>>> origin/master
+
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -20,10 +19,8 @@ public class DepartmentGraphQL {
 
     private final DepartmentService departmentService;
 
-<<<<<<< HEAD
+
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-=======
->>>>>>> origin/master
     @MutationMapping()
     Department departmentCreate(@Argument String name) {
         var depart = new Department();
@@ -32,10 +29,7 @@ public class DepartmentGraphQL {
     }
 
     @QueryMapping()
-<<<<<<< HEAD
     @PreAuthorize("hasAnyRole('ROLE_GUEST','ROLE_USER','ROLE_ADMIN')")
-=======
->>>>>>> origin/master
     List<Department> departments() {
         return departmentService.deparmetns();
     }
